098702acb437972e3f82bc8a67bb42c969814981
Nathalia <110792289+ndamasc@users.noreply.github.com>
2025-03-01 20:54:01 -0400

    acessando json

--- app.py
+++ app.py
@@ -8,6 +8,18 @@ print(response)   ##3 retorna o status code da solicitacao
 
 if response.status_code == 200:
     data_json = response.json()
-    print(data_json)
+    data_restaurante = {}
+    for item in data_json:
+        nome_restaurante = item['Company']
+        if nome_restaurante not in data_restaurante:
+            data_restaurante[nome_restaurante] = []
+
+        data_restaurante[nome_restaurante].append({
+            "item": item['Item'],
+            "preco": item['price'],
+            "descricao" : item['description']
+        })
 else:
     print(f'O erro foi {response.status_code}')
+
+print(data_restaurante[])  
